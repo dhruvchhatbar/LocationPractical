f3d9614a16d1e4820e1bc82882a9b17f5d007b1d
Dhruv <100026417+dhruvchhatbar@users.noreply.github.com>
2025-01-23 19:39:36 +0530

    Initial Commit

---
+++ LocationPractical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationPractical
+//
+//  Created by Dhruv Chhatbar on 17/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
